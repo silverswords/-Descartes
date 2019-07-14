fc5b9de2db9577e81c5d70ac206efdff023abd28
Wbofeng <1464226975@qq.com>
2019-07-14 11:57:51 +0800

    upload provider

--- lib/pages/root/index.dart
+++ lib/pages/root/index.dart
@@ -1,16 +1,22 @@
 import 'package:flutter_web/material.dart';
+import 'package:descartes/utility/provider/provider.dart';
+import 'package:descartes/model/counter.dart';
 
-class Root extends StatefulWidget { 
-  @override 
- _RootPageState createState() => _RootPageState(); 
-}
-
-class _RootPageState extends State<Root> { 
-   @override
+class Root extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('descartes'),
+      ),
+      body: Text(
+        'Value: ${counter.getCounter()}'
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: counter.increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
--- lib/root.dart
+++ lib/root.dart
@@ -1,17 +1,25 @@
 import 'package:flutter_web/material.dart';
 import 'package:descartes/global.dart';
 import 'package:descartes/pages/root/index.dart';
+import 'package:descartes/utility/provider/provider.dart';
+import 'package:descartes/model/counter.dart';
 
 class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: applicationTheme,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Root()
-      }
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(builder: (_) => Counter(0)),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: applicationTheme,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Root()
+        }
+      )
     );
   }
 }
+
